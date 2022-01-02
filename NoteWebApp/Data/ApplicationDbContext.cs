bb244d7ead4daf557fe80e1c89d9a344f928e3f8
coder23 <coder23000@gmail.com>
2022-01-02 20:51:21 +0100

    Creating ApplicationDbContext - create the context - add connection string to the appsetting - add the context to the services - run migration (add-migration) - create the database (update-database)

---
+++ NoteWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoteWebApp.Models;
+
+namespace NoteWebApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
