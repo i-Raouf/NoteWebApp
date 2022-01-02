f0018cb4bc317450b4c28f858c316021c38c127d
coder23 <coder23000@gmail.com>
2022-01-02 20:10:38 +0100

    Creating th e model

---
+++ NoteWebApp/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoteWebApp.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime LastModified { get; set; } = DateTime.Now;
+    }
+}
